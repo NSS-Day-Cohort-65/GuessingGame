dd4c18894cf5b1411c1c273dc8739d4d1af9e226
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-08-21 12:37:05 -0400

    phase 2

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,3 +1,12 @@
-﻿Console.WriteLine("Guess my secret number:");
-string guess = Console.ReadLine();
-Console.WriteLine($"You guessed: {guess}");
+﻿int secretNumber = 42;
+Console.WriteLine("Guess my secret number:");
+int guess = int.Parse(Console.ReadLine());
+if (guess == secretNumber)
+{
+    Console.WriteLine("You guess the secret number!!!");
+}
+else
+{
+    Console.WriteLine("Try again, LOSER!!!");
+}
+
