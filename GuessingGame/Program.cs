b1c2533c63503806f50ec36c2310e93fed3f3ddb
JDOdea <jondavidodea@gmail.com>
2023-08-21 13:30:50 -0500

    phase 7

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,7 +1,40 @@
 ﻿Random r = new Random();
 int secretNumber = r.Next(1,100);
-int maxGuesses = 4;
+int maxGuesses = 0;
 int attemptedGuesses = 0;
+
+Console.WriteLine(@"Please choose your difficulty: 
+Easy
+Medium
+Hard");
+string difficulty = "null";
+while (difficulty == "null")
+{
+    string response = Console.ReadLine().Trim().ToLower();
+    switch (response)
+    {
+        case "easy":
+        difficulty = "Easy";
+        maxGuesses = 8;
+        break;
+
+        case "medium":
+        difficulty = "Medium";
+        maxGuesses = 6;
+        break;
+
+        case "hard":
+        difficulty = "Hard";
+        maxGuesses = 4;
+        break;
+
+        default:
+        Console.WriteLine("Please choose an actual difficulty, stupid.");
+        break;
+    }
+}
+
+
 while (attemptedGuesses < maxGuesses)
 {
   Console.WriteLine($"Guess my secret number. ");
