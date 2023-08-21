96236324dc050838490ef2bb5d7e8e8423a4814d
Josh Baugh <joshbaughmusic@gmail.com>
2023-08-21 11:49:51 -0500

    awesomeness

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -3,7 +3,7 @@ int maxGuesses = 4;
 int attemptedGuesses = 0;
 while (attemptedGuesses < maxGuesses)
 {
-  Console.WriteLine($" Guess ({attemptedGuesses}) - Guess my secret number:");
+  Console.WriteLine($" Guess ({attemptedGuesses + 1}) - Guess my secret number:");
   int guess = int.Parse(Console.ReadLine());
   if (guess == secretNumber)
   {
